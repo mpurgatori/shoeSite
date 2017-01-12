12480ecb94f4b187de475e2b829e85a890b2a89b
Tredis <t.redis@gmail.com>
2017-01-12 09:56:17 -0600

    backend

---
+++ server/comments.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const db = require('APP/db')
+const Comment = db.model('comment')
+
+const {mustBeLoggedIn, selfOnly,} = require('./auth.filters')
+
+module.exports = require('express').Router()
+
+	.get('/:userId', mustBeLoggedIn, selfOnly("edit your own comments."), (req, res, next) => 
+		Comment.findAll(where:{userId: req.params.userId})
+		.then(comments => res.json(comments))
+		.catch(next))
+
+	.put('/:id', (req, res, next) =>
+		Comment.update(req.body, {where: {id: req.params.id}, returning: true})
+		.then(user => res.json(user[1][0]))
+		.catch(next))
+
+	.post('/', (req, res, next) =>
+		User.create(req.body)
+		.then(user => res.json(user))
+		.catch(next))
+
+	.delete('/:id', (req, res, next) =>
+		User.destroy(where: {id: req.params.id})
+		.then(user => res.json(user))
+		.catch(next))
+	
---
+++ server/orders.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const db = require('APP/db')
+const Comment = db.model('comment')
+
+const {mustBeLoggedIn, selfOnly,} = require('./auth.filters')
+
+module.exports = require('express').Router()
+	
+	.get('/orders/:userId', mustBeLoggedIn, selfOnly("see your own orders."), (req, res, next) => 
+		Order.findAll({where: {userId: req.params.userId}, order: 'date DESC'})
+		.then(user => res.json(user))
+		.catch(next))
