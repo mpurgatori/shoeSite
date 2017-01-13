d9855f89164a4363f73ce2bd4a17ac30e93f977e
acenorth <north.andy@gmail.com>
2017-01-13 13:37:15 -0600

    route, no button

--- app/components/ShoeOrderCard.js
+++ app/components/ShoeOrderCard.js
@@ -2,7 +2,6 @@ import React from 'react';
 import {Link} from 'react-router';
 
 export default function (props) {
-  console.log("PROPSSSSS:", props);
   let order = props.order;
   let shoe = props.shoe;
 
