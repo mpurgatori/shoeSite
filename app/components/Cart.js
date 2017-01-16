c3478ff9b2692a791c24f23f96b6021fd615de79
acenorth <north.andy@gmail.com>
2017-01-16 16:01:30 -0600

    cart fixes

--- app/components/Cart.js
+++ app/components/Cart.js
@@ -2,29 +2,26 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import ShoeOrderCard from './ShoeOrderCard';
 
-		
-
 export default (props) => {
-		const arrToMap = props.pendingOrder.shoe_inventories;
-		const order = props.pendingOrder;
-		const removeShoe = props.removeShoe;
+	const arrToMap = props.pendingOrder.shoe_inventories;
+	const order = props.pendingOrder;
+	const removeShoe = props.removeShoe;
 
-		return (
-
-			<div>
-				{ arrToMap ? (
-					arrToMap.map( shoe => {
-						<div>
+	return (
+		<div>
+			{ arrToMap ? (
+				arrToMap.map( shoe => {
+					return (<div>
 						<ShoeOrderCard key={shoe.id} shoe={shoe} order={order} removeShoe={ removeShoe } />
 						<button onClick={() => removeShoe(order.id, shoe.id)}> x </button>
-						</div>
-						})
-					) : null
-				}
-				<a href="/confirm">Place Order</a>
-			</div>
-			)
-		
-	}
+					</div>)
+					})
+				) : null
+			}
+			<a href="/confirm">Place Order</a>
+		</div>
+		)
+	
+}
 
 
--- app/main.jsx
+++ app/main.jsx
@@ -25,7 +25,7 @@ const onShoeEnter = function (nextRouterState) {
   store.dispatch(getShoeModel(shoemodelId));
 };
 
-const exampleUser = {id:1};
+const exampleUser = {id:2};
 
 render (
   <Provider store={store}>
